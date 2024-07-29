6e6348662d89337d004714d132c3b0989f32f146
zhangzexin <zzx-nuc@qq.com>
2024-07-29 15:08:59 +0800

    feat(页面): 资源管理模块基础页面修改

---
+++ src/views/ota/resource/hook.tsx
@@ -0,0 +1,203 @@
+import { computed, onMounted, reactive, ref } from "vue";
+import type { PaginationProps } from "@pureadmin/table";
+import { FormInstance, FormRules } from "element-plus";
+
+export function useResource() {
+  // ----变量定义-----
+  const queryForm = reactive({
+    name: "",
+    code: "",
+    status: ""
+  });
+  const dataList = ref([]);
+  const loading = ref(true);
+  const dialogFormVisible = ref(false);
+  const title = ref("");
+  const pagination = reactive<PaginationProps>({
+    total: 0,
+    pageSize: 10,
+    currentPage: 1,
+    background: true
+  });
+  const addForm = reactive({
+    value: {
+      id: null,
+      name: "",
+      code: "",
+      status: "",
+      description: ""
+    }
+  });
+  const rules = reactive<FormRules>({
+    name: [{ required: true, message: "角色名称必填", trigger: "blur" }],
+    code: [{ required: true, message: "角色编码必填", trigger: "blur" }]
+  });
+  const columns: TableColumnList = [
+    {
+      type: "selection",
+      width: 55,
+      align: "left"
+    },
+    {
+      label: "序号",
+      type: "index",
+      width: 70
+    },
+    {
+      label: "软件名称",
+      prop: "softwareName",
+      minWidth: 100
+    },
+    {
+      label: "安装包名称",
+      prop: "pkgName",
+      minWidth: 120
+    },
+    {
+      label: "类型",
+      prop: "type",
+      minWidth: 120
+    },
+    {
+      label: "版本号",
+      prop: "version",
+      minWidth: 120
+    },
+    {
+      label: "文件大小",
+      prop: "fileSize",
+      minWidth: 120
+    },
+    {
+      label: "MD5值",
+      prop: "md5",
+      minWidth: 120
+    },
+    {
+      label: "上传时间",
+      minWidth: 180,
+      prop: "createTime"
+    },
+    {
+      label: "备注",
+      prop: "remark",
+      minWidth: 150
+    },
+    {
+      label: "操作",
+      fixed: "right",
+      width: 180,
+      slot: "operation"
+    }
+  ];
+  const buttonClass = computed(() => {
+    return [
+      "!h-[20px]",
+      "reset-margin",
+      "!text-gray-500",
+      "dark:!text-white",
+      "dark:hover:!text-primary"
+    ];
+  });
+
+  // -----方法定义---
+  // 修改
+  function handleUpdate(row) {
+    console.log(row);
+    const roleInfo = JSON.stringify(row);
+    addForm.value = JSON.parse(roleInfo);
+    openDia("修改资源");
+  }
+  // 删除
+  function handleDelete(row) {
+    console.log(row);
+  }
+
+  function handleSizeChange(val: number) {
+    console.log(`${val} items per page`);
+  }
+
+  function handleCurrentChange(val: number) {
+    console.log(`current page: ${val}`);
+  }
+
+  function handleSelectionChange(val) {
+    console.log("handleSelectionChange", val);
+  }
+  // 查询
+  async function onSearch() {
+    loading.value = true;
+    console.log("查询资源信息");
+    setTimeout(() => {
+      loading.value = false;
+    }, 500);
+  }
+
+  const resetForm = formEl => {
+    if (!formEl) return;
+    formEl.resetFields();
+    onSearch();
+  };
+  // 取消
+  function cancel(formEl) {
+    addForm.value = {
+      id: null,
+      name: "",
+      code: "",
+      status: "",
+      description: ""
+    };
+    resetForm(formEl);
+    dialogFormVisible.value = false;
+  }
+  // 保存
+  const submitForm = async (formEl: FormInstance | undefined) => {
+    if (!formEl) return;
+    await formEl.validate((valid, fields) => {
+      if (valid) {
+        console.log(addForm.value);
+        if (addForm.value.id) {
+          // 修改
+          console.log("修改资源");
+        } else {
+          // 新增
+          console.log("新增资源");
+        }
+      } else {
+        console.log("error submit!", fields);
+      }
+    });
+  };
+  // 打开弹框
+  function openDia(param) {
+    dialogFormVisible.value = true;
+    title.value = param;
+  }
+
+  onMounted(() => {
+    onSearch();
+  });
+
+  return {
+    queryForm,
+    dataList,
+    loading,
+    dialogFormVisible,
+    title,
+    pagination,
+    addForm,
+    rules,
+    columns,
+    buttonClass,
+    onSearch,
+    resetForm,
+    handleUpdate,
+    handleDelete,
+    handleSizeChange,
+    handleCurrentChange,
+    handleSelectionChange,
+    cancel,
+    submitForm,
+    openDia
+  };
+}
--- src/views/ota/resource/index.vue
+++ src/views/ota/resource/index.vue
@@ -1,3 +1,172 @@
+<script setup lang="ts">
+import { ref } from "vue";
+import { FormInstance } from "element-plus";
+import { useResource } from "@/views/ota/resource/hook";
+import { useRenderIcon } from "@/components/ReIcon/src/hooks";
+import Search from "@iconify-icons/ep/search";
+import Refresh from "@iconify-icons/ep/refresh";
+import AddFill from "@iconify-icons/ri/add-circle-line";
+import EditPen from "@iconify-icons/ep/edit-pen";
+import Delete from "@iconify-icons/ep/delete";
+import { PureTableBar } from "@/components/RePureTableBar";
+
+defineOptions({
+  name: "Resource"
+});
+
+const formRef = ref();
+const addFormRef = ref<FormInstance>();
+
+const {
+  queryForm,
+  loading,
+  columns,
+  dataList,
+  pagination,
+  dialogFormVisible,
+  title,
+  addForm,
+  rules,
+  cancel,
+  submitForm,
+  openDia,
+  onSearch,
+  resetForm,
+  handleUpdate,
+  handleDelete,
+  handleSizeChange,
+  handleCurrentChange,
+  handleSelectionChange
+} = useResource();
+</script>
 <template>
-  <h1>资源管理</h1>
+  <div class="main">
+    <el-form
+      ref="formRef"
+      :inline="true"
+      :model="queryForm"
+      class="bg-bg_color w-[99/100] pl-8 pt-4"
+    >
+      <el-form-item label="资源名称：" prop="name">
+        <el-input
+          v-model="queryForm.name"
+          placeholder="请输入资源名称"
+          clearable
+          class="!w-[180px]"
+        />
+      </el-form-item>
+      <el-form-item>
+        <el-button
+          type="primary"
+          :icon="useRenderIcon(Search)"
+          :loading="loading"
+          @click="onSearch"
+        >
+          搜索
+        </el-button>
+        <el-button :icon="useRenderIcon(Refresh)" @click="resetForm(formRef)">
+          重置
+        </el-button>
+      </el-form-item>
+    </el-form>
+
+    <PureTableBar title="资源列表" @refresh="onSearch">
+      <template #buttons>
+        <el-button
+          type="primary"
+          :icon="useRenderIcon(AddFill)"
+          @click="openDia('新增资源')"
+        >
+          新增
+        </el-button>
+      </template>
+      <template v-slot="{ size, checkList }">
+        <pure-table
+          border
+          align-whole="center"
+          showOverflowTooltip
+          table-layout="auto"
+          :loading="loading"
+          :size="size"
+          :data="dataList"
+          :columns="columns"
+          :checkList="checkList"
+          :pagination="pagination"
+          :paginationSmall="size === 'small'"
+          :header-cell-style="{
+            background: 'var(--el-table-row-hover-bg-color)',
+            color: 'var(--el-text-color-primary)'
+          }"
+          @selection-change="handleSelectionChange"
+          @page-size-change="handleSizeChange"
+          @page-current-change="handleCurrentChange"
+        >
+          <template #operation="{ row }">
+            <el-button
+              class="reset-margin"
+              link
+              type="primary"
+              :size="size"
+              :icon="useRenderIcon(EditPen)"
+              @click="handleUpdate(row)"
+            >
+              修改
+            </el-button>
+            <el-popconfirm title="是否确认删除?" @confirm="handleDelete(row)">
+              <template #reference>
+                <el-button
+                  class="reset-margin"
+                  link
+                  type="primary"
+                  :size="size"
+                  :icon="useRenderIcon(Delete)"
+                >
+                  删除
+                </el-button>
+              </template>
+            </el-popconfirm>
+          </template>
+        </pure-table>
+      </template>
+    </PureTableBar>
+
+    <el-dialog v-model="dialogFormVisible" :title="title">
+      <el-form
+        ref="addFormRef"
+        :model="addForm.value"
+        :inline="true"
+        :rules="rules"
+        label-width="100px"
+      >
+        <el-form-item label="软件名称" prop="name">
+          <el-input v-model="addForm.value.name" placeholder="请输入软件名称" />
+        </el-form-item>
+
+        <el-form-item label="安装包名称" prop="code">
+          <el-input v-model="addForm.value.code" placeholder="请输入角色编码" />
+        </el-form-item>
+
+        <el-form-item label="类型" prop="description">
+          <el-input
+            v-model="addForm.value.description"
+            placeholder="请输入类型"
+          />
+        </el-form-item>
+      </el-form>
+      <template #footer>
+        <span class="dialog-footer">
+          <el-button @click="cancel(addFormRef)">取消</el-button>
+          <el-button type="primary" @click="submitForm(addFormRef)"
+            >确认</el-button
+          >
+        </span>
+      </template>
+    </el-dialog>
+  </div>
 </template>
+
+<style scoped lang="scss">
+:deep(.el-dropdown-menu__item i) {
+  margin: 0;
+}
+</style>
