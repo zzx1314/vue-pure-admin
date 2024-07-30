064e0e65e10f494f10a81cdcb0802775f09bb508
zhangzexin <zzx-nuc@qq.com>
2024-07-30 09:52:28 +0800

    feat(上传): 添加上传组件

--- src/views/nested/menu1/menu1-1/index.vue
+++ src/views/nested/menu1/menu1-1/index.vue
@@ -202,9 +202,44 @@ const handleRemoveFile = uploadFile => {
     fileUploadChunkQueue[uploadFile.uid] = undefined;
   }
 };
+
+import { reactive } from "vue";
+
+const formInline = reactive({
+  user: "",
+  region: "",
+  date: ""
+});
 </script>
 <template>
   <el-card style="width: 80%; margin: 80px auto" header="文件分片上传">
+    <el-form :inline="true" :model="formInline" class="demo-form-inline">
+      <el-form-item label="Approved by">
+        <el-input
+          v-model="formInline.user"
+          placeholder="Approved by"
+          clearable
+        />
+      </el-form-item>
+      <el-form-item label="Activity zone">
+        <el-select
+          v-model="formInline.region"
+          placeholder="Activity zone"
+          clearable
+        >
+          <el-option label="Zone one" value="shanghai" />
+          <el-option label="Zone two" value="beijing" />
+        </el-select>
+      </el-form-item>
+      <el-form-item label="Activity time">
+        <el-date-picker
+          v-model="formInline.date"
+          type="date"
+          placeholder="Pick a date"
+          clearable
+        />
+      </el-form-item>
+    </el-form>
     <el-upload
       class="upload-demo"
       drag
