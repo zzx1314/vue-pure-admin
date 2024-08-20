77ca8ba83230a8d2f06e2754cf511f4764c140dc
zhangzexin <zzx-nuc@qq.com>
2024-08-20 13:52:28 +0800

    feat(ota): 前端打包修改

--- build/info.ts
+++ build/info.ts
@@ -7,7 +7,7 @@ import boxen, { type Options as BoxenOptions } from "boxen";
 dayjs.extend(duration);
 
 const welcomeMessage = gradientString("cyan", "magenta").multiline(
-  `您好! 欢迎使用 pure-admin 开源项目\n我们为您精心准备了下面两个贴心的保姆级文档\nhttps://pure-admin.github.io/pure-admin-doc\nhttps://pure-admin-utils.netlify.app`
+  `前端打包。。。`
 );
 
 const boxenOptions: BoxenOptions = {
