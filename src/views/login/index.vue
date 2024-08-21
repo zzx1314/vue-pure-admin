09cb2f0f85f437cd14b03f3421d647b5a9bdb28c
zhangzexin <zzx-nuc@qq.com>
2024-08-21 16:01:31 +0800

    feat(系统): 添加版权信息

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -230,7 +230,7 @@ watch(loginDay, value => {
     <div
       class="w-full flex-c absolute bottom-3 text-sm text-[rgba(0,0,0,0.6)] dark:text-[rgba(220,220,242,0.8)]"
     >
-      Copyright © 2020-present
+      Copyright © 华郅技术
       <a
         class="hover:text-primary"
         href="https://github.com/pure-admin"
