59a74c88b26d9a2bc94be239ef525b4deaf85f86
zhangzexin <zzx-nuc@qq.com>
2024-08-28 15:50:35 +0800

    feat(登录): 去除默认密码

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -57,8 +57,8 @@ const { title, getDropdownItemStyle, getDropdownItemClass } = useNav();
 const { locale, translationCh, translationEn } = useTranslationLang();
 
 const ruleForm = reactive({
-  username: "sysadmin",
-  password: "Aa123456",
+  username: "",
+  password: "",
   verifyCode: ""
 });
 
