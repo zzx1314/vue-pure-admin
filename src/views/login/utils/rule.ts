559a5741ae81797fca34e1c65a19158ad5c4d1f7
zhangzexin <zzx-nuc@qq.com>
2024-09-04 16:46:40 +0800

    feat(登录): 先去除密码校验

--- src/views/login/utils/rule.ts
+++ src/views/login/utils/rule.ts
@@ -112,8 +112,6 @@ const updateRules = reactive<FormRules>({
       validator: (rule, value, callback) => {
         if (value === "") {
           callback(new Error(transformI18n($t("login.purePassWordReg"))));
-        } else if (!REGEXP_PWD.test(value)) {
-          callback(new Error(transformI18n($t("login.purePassWordRuleReg"))));
         } else {
           callback();
         }
