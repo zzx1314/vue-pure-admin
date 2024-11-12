2b624a98070ae6348357b19e43e4772a04242577
zhangzexin <zzx-nuc@qq.com>
2024-11-12 17:17:05 +0800

    feat: 添加图标

--- src/components/ReIcon/src/offlineIcon.ts
+++ src/components/ReIcon/src/offlineIcon.ts
@@ -48,6 +48,7 @@ import FolderShield2Line from "@iconify-icons/ri/folder-shield-2-line";
 import FileLockLine from "@iconify-icons/ri/file-lock-line";
 import ShieldFlashLine from "@iconify-icons/ri/shield-flash-line";
 import SlideshowLine from "@iconify-icons/ri/slideshow-line";
+import bookletLine from "@iconify-icons/ri/booklet-line";
 
 addIcon("ri:bookmark-2-line", Tag);
 addIcon("ri:file-ppt-2-line", Ppt);
@@ -77,6 +78,7 @@ addIcon("ri:folder-shield-2-line", FolderShield2Line);
 addIcon("ri:file-lock-line", FileLockLine);
 addIcon("ri:shield-flash-line", ShieldFlashLine);
 addIcon("ri:slideshow-line", SlideshowLine);
+addIcon("ri:booklet-line", bookletLine);
 
 import Device from "@iconify-icons/tdesign/system-device";
 import DeviceOne from "@iconify-icons/tdesign/device";
