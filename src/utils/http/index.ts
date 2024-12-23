4a9c31c68b604fb7cac0ca13de1a61106663e70f
zhangzexin <zzx-nuc@qq.com>
2024-12-23 14:30:48 +0800

    feat: 401提示问题修改

--- src/utils/http/index.ts
+++ src/utils/http/index.ts
@@ -138,7 +138,7 @@ class PureHttp {
         if (error.response.status === 500) {
           message("服务器错误", { type: "error" });
         } else if (error.response.status === 401) {
-          message("系统用户名或密码错误", { type: "error" });
+          message("当前用户没有分配权限", { type: "error" });
         } else if (
           error.response.status === 400 &&
           error.response.data &&
