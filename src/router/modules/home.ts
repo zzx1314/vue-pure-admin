df8c18ae5a3d3ba7604a702851e5bb8185401e16
zhangzexin <zzx-nuc@qq.com>
2024-12-24 14:18:22 +0800

    feat: 设置首页路由

--- src/router/modules/home.ts
+++ src/router/modules/home.ts
@@ -17,7 +17,7 @@ export default {
     {
       path: "/welcome",
       name: "Welcome",
-      component: () => import("@/views/welcome/index.vue"),
+      component: () => import("@/views/obusdevice/device/index.vue"),
       meta: {
         title: $t("menus.pureHome"),
         showLink: VITE_HIDE_HOME === "true" ? false : true
