aa7ef7e3b862c41b672ef443e19e9249296c0d2b
zhangzexin <zzx-nuc@qq.com>
2025-01-18 15:58:05 +0800

    fix: 解决密码修改显示框的问题

--- src/layout/components/lay-sidebar/UserInfoForm.vue
+++ src/layout/components/lay-sidebar/UserInfoForm.vue
@@ -63,39 +63,39 @@ function closeDialog() {
 }
 
 const cancel = formEl => {
+  clearForm();
+  resetForm(formEl);
+  closeDialog();
+};
+
+function clearForm() {
   addForm.value = {
     id: null,
     password: "",
     newpassword: "",
     newpassword1: ""
   };
-  resetForm(formEl);
-  closeDialog();
-};
+}
 
 const addFormInfo = async (formEl: FormInstance | undefined) => {
   if (!formEl) return;
   await formEl.validate((valid, fields) => {
     if (valid) {
       console.log("修改密码");
-      addForm.value.password = aesUtils.encode(addForm.value.password, "");
-      addForm.value.newpassword = aesUtils.encode(
-        addForm.value.newpassword,
-        ""
-      );
-      addForm.value.newpassword1 = aesUtils.encode(
-        addForm.value.newpassword1,
-        ""
-      );
-      addForm.value.id =
-        storageLocal().getItem<DataInfo<number>>(userKey).user_id;
+      let param = {
+        ...addForm.value
+      };
+      param.password = aesUtils.encode(addForm.value.password, "");
+      param.newpassword = aesUtils.encode(addForm.value.newpassword, "");
+      param.newpassword1 = aesUtils.encode(addForm.value.newpassword1, "");
+      param.id = storageLocal().getItem<DataInfo<number>>(userKey).user_id;
       console.log(addForm.value);
-      updatePassword(addForm.value).then(res => {
+      updatePassword(param).then(res => {
         if (res.code === SUCCESS) {
           ElMessage.success("修改成功");
           closeDialog();
         } else {
-          ElMessage.error("修改失败");
+          ElMessage.error(res.msg);
         }
       });
     } else {
