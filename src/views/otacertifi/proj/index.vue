e34ff2cfe3906d0cc328aae2abba928d8f0b1ff1
zhangzexin <zzx-nuc@qq.com>
2025-02-07 19:36:29 +0800

    fix: 项目创建成功将不允许修改，项目编码和客户名称

--- src/views/otacertifi/proj/index.vue
+++ src/views/otacertifi/proj/index.vue
@@ -213,6 +213,7 @@ defineOptions({
           <el-input
             v-model="addForm.value.projCode"
             style="width: 200px"
+            :disabled="title == '修改'"
             placeholder="请输入项目编码"
           />
         </el-form-item>
@@ -222,6 +223,7 @@ defineOptions({
             clearable
             placeholder="请选择客户"
             style="width: 200px"
+            :disabled="title == '修改'"
           >
             <el-option
               v-for="item in customerList"
