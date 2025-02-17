89fa636fd46a56ffe41ef95db120ac858eeeb701
zhangzexin <zzx-nuc@qq.com>
2025-02-17 09:05:22 +0800

    feat: 去除mock插件

--- build/plugins.ts
+++ build/plugins.ts
@@ -13,7 +13,7 @@ import removeConsole from "vite-plugin-remove-console";
 import { themePreprocessorPlugin } from "@pureadmin/theme";
 import VueI18nPlugin from "@intlify/unplugin-vue-i18n/vite";
 import { genScssMultipleScopeVars } from "../src/layout/theme";
-import { vitePluginFakeServer } from "vite-plugin-fake-server";
+// import { vitePluginFakeServer } from "vite-plugin-fake-server";
 
 export function getPluginsList(
   VITE_CDN: boolean,
@@ -38,12 +38,12 @@ export function getPluginsList(
      */
     removeNoMatch(),
     // mock支持
-    vitePluginFakeServer({
+    /*vitePluginFakeServer({
       logger: false,
       include: "mock",
       infixName: false,
       enableProd: true
-    }),
+    }),*/
     // 自定义主题
     themePreprocessorPlugin({
       scss: {
