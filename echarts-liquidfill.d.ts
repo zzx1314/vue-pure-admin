20668c9ad340679282cf1d2a253b4536e1558915
zhangzexin <zzx-nuc@qq.com>
2025-02-19 11:41:32 +0800

    feat: 水球图调试，去除多余代码

--- echarts-liquidfill.d.ts
+++
@@ -1,6 +0,0 @@
-declare module "echarts-liquidfill" {
-  import type { EChartsExtensionModule } from "echarts";
-
-  export const version: string;
-  export function install(registers: EChartsExtensionModule): void;
-}
