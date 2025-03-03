9aa4fe45d7a18b47755f8a366caab5bb7fb9e4b7
zhangzexin <zzx-nuc@qq.com>
2025-03-03 17:30:10 +0800

    feat: 新增字典管理

---
+++ src/views/system/sysDict/form.tsx
@@ -0,0 +1,108 @@
+// form表单
+import type { PlusColumn } from "plus-pro-components";
+
+export function useDictForm() {
+  const columnsForm: PlusColumn[] = [
+    {
+      label: "类型",
+      prop: "dictType",
+      valueType: "select",
+      options: [
+        {
+          label: "用户类",
+          value: "用户类"
+        },
+        {
+          label: "系统类",
+          value: "系统类"
+        }
+      ],
+      formItemProps: {
+        style: {
+          width: "50%"
+        }
+      }
+    },
+    {
+      label: "配置类型",
+      prop: "type",
+      formItemProps: {
+        style: {
+          width: "50%"
+        }
+      }
+    },
+    {
+      label: "配置描述",
+      prop: "description",
+      formItemProps: {
+        style: {
+          width: "50%"
+        }
+      }
+    },
+    {
+      label: "备注",
+      prop: "remarks",
+      width: "10px",
+      valueType: "textarea"
+    }
+  ];
+
+  const columnsQueryForm: PlusColumn[] = [
+    {
+      label: "配置类型",
+      prop: "dictType",
+      valueType: "copy",
+      colProps: {
+        span: 5
+      }
+    },
+    {
+      label: "配置描述",
+      prop: "description",
+      colProps: {
+        span: 5
+      }
+    },
+    {
+      label: "开始时间",
+      prop: "beginTime",
+      valueType: "date-picker",
+      type: "date",
+      formItemProps: {
+        style: {
+          width: "250px"
+        }
+      },
+      fieldProps: {
+        type: "date",
+        valueFormat: "YYYY-MM-DD HH:mm:ss"
+      },
+      colProps: {
+        span: 5
+      }
+    },
+    {
+      label: "结束时间",
+      prop: "endTime",
+      valueType: "date-picker",
+      formItemProps: {
+        style: {
+          width: "250px"
+        }
+      },
+      fieldProps: {
+        type: "date",
+        valueFormat: "YYYY-MM-DD"
+      },
+      colProps: {
+        span: 5
+      }
+    }
+  ];
+  return {
+    columnsForm,
+    columnsQueryForm
+  };
+}
