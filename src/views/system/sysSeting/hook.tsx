845408839facf84c68d4580e41868f55e989f961
zhangzexin <zzx-nuc@qq.com>
2025-03-04 18:47:41 +0800

    fix: 修改配置提示修改

--- src/views/system/sysSeting/hook.tsx
+++ src/views/system/sysSeting/hook.tsx
@@ -66,7 +66,7 @@ export function useSysSeting() {
     console.log("保存", ref);
     updateSafePolicy(addForm.value).then(res => {
       if (res.code === SUCCESS) {
-        ElMessage.success("保存成功，新的配置将会在下次生效！");
+        ElMessage.success("保存成功，新的配置将会在下次登录生效！");
       } else {
         ElMessage.error(res.msg);
       }
