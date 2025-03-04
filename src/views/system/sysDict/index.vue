20df3a53f4741e506aaae2cc261a182d579bfbc5
zhangzexin <zzx-nuc@qq.com>
2025-03-04 14:16:16 +0800

    fix: 修改字典阈值修改

--- src/views/system/sysDict/index.vue
+++ src/views/system/sysDict/index.vue
@@ -290,10 +290,15 @@ const {
         >
           <template #edit="{ row }">
             <vxe-number-input
-              v-if="row.label === '心跳时间'"
+              v-if="
+                row.label === '心跳时间' ||
+                row.label === 'CPU阈值' ||
+                row.label === '磁盘阈值' ||
+                row.label === '内存阈值'
+              "
               v-model="row.value"
               :min="10"
-              :max="300"
+              :max="row.label === '心跳时间' ? 300 : 100"
             />
             <vxe-input v-else v-model="row.value" :disabled="disabledValue" />
           </template>
