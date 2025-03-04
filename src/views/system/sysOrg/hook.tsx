c570f7d4ace8bf2dd0bac19548522c493bb6fa88
zhangzexin <zzx-nuc@qq.com>
2025-03-04 16:40:41 +0800

    fix: 组织管理固定修改

--- src/views/system/sysOrg/hook.tsx
+++ src/views/system/sysOrg/hook.tsx
@@ -61,7 +61,8 @@ export function useDept() {
       label: "部门名称",
       prop: "name",
       width: 180,
-      align: "left"
+      align: "left",
+      fixed: "left"
     },
     {
       label: "排序",
