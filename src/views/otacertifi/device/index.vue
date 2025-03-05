25fc0d74edb09347d0d20d8c0e33af2d34a13a7b
zhangzexin <zzx-nuc@qq.com>
2025-03-05 14:55:42 +0800

    feat: 新增设备状态

--- src/views/otacertifi/device/index.vue
+++ src/views/otacertifi/device/index.vue
@@ -88,7 +88,7 @@ const {
           <el-option label="系统架构异常" value="系统架构异常" />
           <el-option label="用户名变化" value="用户名变化" />
           <el-option label="license时间发生变化" value="license时间发生变化" />
-          <el-option label="证书过期" value="证书过期" />
+          <el-option label="授权证书过期" value="授权证书过期" />
           <el-option label="获取磁盘信息失败" value="获取磁盘信息失败" />
           <el-option label="解码license失败" value="解码license失败" />
           <el-option label="校验license失败" value="校验license失败" />
