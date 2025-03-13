57768693405f05aeb21efb839895455e02910560
zhangzexin <zzx-nuc@qq.com>
2025-03-13 17:56:52 +0800

    feat: 添加服务器时间

--- src/layout/components/lay-sidebar/SysDate.vue
+++ src/layout/components/lay-sidebar/SysDate.vue
@@ -1,10 +1,56 @@
 <script setup lang="ts">
-import { ref } from "vue";
+import { onMounted, onUnmounted, ref } from "vue";
+
+const getWebSocketUrl = () => {
+  let protocol = "ws://";
+  if (window.location.protocol === "https:") {
+    protocol = "wss://";
+  }
+  return protocol + window.location.host + "/notice";
+};
 
 const nowData = ref("");
-setInterval(() => {
-  nowData.value = new Date().toLocaleString();
-}, 1000);
+let webSocket: WebSocket | null = null;
+
+onMounted(() => {
+  // 建立 WebSocket 连接
+  webSocket = new WebSocket(getWebSocketUrl()); // 替换为你的 WebSocket 服务器地址
+
+  webSocket.onopen = () => {
+    console.log("WebSocket connection established");
+  };
+
+  webSocket.onmessage = event => {
+    // console.log("Message from server:", event.data);
+    if (event.data) {
+      nowData.value = JSON.parse(event.data).date;
+    }
+  };
+
+  webSocket.onerror = error => {
+    console.error("WebSocket error:", error);
+  };
+
+  webSocket.onclose = () => {
+    console.log("WebSocket connection closed");
+  };
+
+  // 定时任务，每秒更新时间并发送信息
+  const intervalId = setInterval(() => {
+    // 发送信息到 WebSocket 服务器
+    if (webSocket && webSocket.readyState === WebSocket.OPEN) {
+      webSocket.send(JSON.stringify({ type: "heartbeat" }));
+    }
+  }, 1000);
+
+  // 清除定时器和 WebSocket 连接
+  onUnmounted(() => {
+    clearInterval(intervalId);
+    if (webSocket) {
+      webSocket.close();
+    }
+  });
+});
 </script>
 
 <template>
--- vite.config.ts
+++ vite.config.ts
@@ -29,6 +29,12 @@ export default ({ mode }: ConfigEnv): UserConfigExport => {
           // 这里填写后端地址
           target: "http://127.0.0.1:58088/",
           changeOrigin: true
+        },
+        "/notice": {
+          // 假设 WebSocket 的路径是 /ws
+          target: "ws://127.0.0.1:58088/", // WebSocket 地址
+          changeOrigin: true,
+          ws: true // 启用 WebSocket 代理
         }
       },
       // 预热文件以提前转换和缓存结果，降低启动期间的初始页面加载时长并防止转换瀑布
