f33b875cb0b185e2885cdd15a9c1ef6ef608b58d
zhangzexin <zzx-nuc@qq.com>
2025-03-14 11:57:34 +0800

    fix(同步): 去除系统设置

--- src/layout/components/lay-sidebar/NavMix.vue
+++ src/layout/components/lay-sidebar/NavMix.vue
@@ -120,7 +120,7 @@ watch(
       <!-- 全屏 -->
       <LaySidebarFullScreen id="full-screen" />
       <!-- 退出登录 -->
-      <el-dropdown trigger="click">
+      <el-dropdown trigger="click" class="mr-4">
         <span class="el-dropdown-link navbar-bg-hover select-none">
           <p v-if="username" class="dark:text-white">{{ username }}</p>
         </span>
@@ -143,13 +143,13 @@ watch(
           </el-dropdown-menu>
         </template>
       </el-dropdown>
-      <span
+      <!--      <span
         class="set-icon navbar-bg-hover"
         :title="t('buttons.pureOpenSystemSet')"
         @click="onPanel"
       >
         <IconifyIconOffline :icon="Setting" />
-      </span>
+      </span>-->
     </div>
 
     <UserInfoForm
