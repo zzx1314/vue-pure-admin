02603f422423214b8ae606823a564074f59ffcac
zhangzexin <zzx-nuc@qq.com>
2025-03-14 19:34:08 +0800

    fix(同步): 获取token只在session中获取

--- src/utils/auth.ts
+++ src/utils/auth.ts
@@ -34,9 +34,7 @@ export const multipleTabsKey = "multiple-tabs";
 /** 获取`token` */
 export function getToken(): DataInfo<number> {
   // 此处与`TokenKey`相同，此写法解决初始化时`Cookies`中不存在`TokenKey`报错
-  return Cookies.get(TokenKey)
-    ? JSON.parse(Cookies.get(TokenKey))
-    : storageSession().getItem(userKey);
+  return storageSession().getItem(userKey);
 }
 
 /**
