b477a7538ebae9cd2368c35184955ae55d389585
zhangzexin <zzx-nuc@qq.com>
2025-03-17 14:53:01 +0800

    fix(同步): 弹框同步修改

--- src/views/system/sysOrg/index.vue
+++ src/views/system/sysOrg/index.vue
@@ -130,7 +130,7 @@ const {
           v-if="hasAuth('org_add')"
           type="primary"
           :icon="useRenderIcon(AddFill)"
-          @click="openDia('添加组织')"
+          @click="openDia('新增组织')"
         >
           新增
         </el-button>
