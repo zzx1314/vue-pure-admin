0f14f2637757cbd8587d4bd1eabd26719bceb507
zhangzexin <zzx-nuc@qq.com>
2025-03-19 14:38:34 +0800

    feat(菜单管理): 同步，添加限制逻辑

--- src/views/system/sysMenu/index.vue
+++ src/views/system/sysMenu/index.vue
@@ -45,14 +45,14 @@ const {
       @refresh="onSearch"
     >
       <template #buttons>
-        <el-button
+        <!--        <el-button
           v-if="hasAuth('menu_add')"
           type="primary"
           :icon="useRenderIcon(AddFill)"
           @click="openDia('新增菜单', addFormRef)"
         >
           新增
-        </el-button>
+        </el-button>-->
       </template>
       <template v-slot="{ size, checkList, dynamicColumns }">
         <pure-table
@@ -189,6 +189,7 @@ const {
         >
           <el-input
             v-model="addForm.value.permission"
+            disabled
             placeholder="请输入权限标识"
           />
         </el-form-item>
